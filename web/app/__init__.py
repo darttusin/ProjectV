6adc46f4c69e5de611ad551e8fcd9a7f788f0c5b
Ivan <ivanchepikov444@gmail.com>
2022-05-27 22:25:45 +0300

    Add upload video but without error in file extension

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -3,5 +3,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+UPLOAD_FOLDER = '../tracker/data/video'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes
--- web/app/routes.py
+++ web/app/routes.py
@@ -1,11 +1,23 @@
-from flask import render_template, request, flash, redirect
+from flask import render_template, request, flash, redirect, url_for, Flask
 from app import app
+import os
+from werkzeug.utils import secure_filename
+
 
 name = "Unknown User"
 
 
 @app.route ( '/', methods=['GET', 'POST'] )
 def home():
+    if request.method == 'POST':
+
+        file = request.files['file']
+        if file.filename[file.filename.find('.') + 1:len(file.filename)] != 'mp4':
+            return render_template('#', title='#')
+        if file.filename == '':
+            return redirect(request.url)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'hype.mp4'))
+        return redirect(url_for('index'))
     return render_template ( 'home.html', title='Home')
 
 @app.route ( '/index', methods=['GET', 'POST'] )
@@ -18,3 +30,20 @@ def index():
         user = {'username': username}
         name = username
     return render_template ( 'index.html', title='Stream', user=user )
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'hype.mp4'))
+        return redirect(url_for('index'))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Загрузите файл с расширением mp4 </h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
