85bed8704ab78258608d115ade07dccb24ff514b
PressFK <alexsapr2003@gmail.com>
2022-05-27 23:58:52 +0300

    Add page when file with  bad resolution

--- web/app/routes.py
+++ web/app/routes.py
@@ -13,7 +13,7 @@ def home():
 
         file = request.files['file']
         if file.filename[file.filename.find('.') + 1:len(file.filename)] != 'mp4':
-            return render_template('#', title='#')
+            return render_template('error.html', title='Home')
         if file.filename == '':
             return redirect(request.url)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'hype.mp4'))
@@ -22,28 +22,4 @@ def home():
 
 @app.route ( '/index', methods=['GET', 'POST'] )
 def index():
-    global name
-    username = request.form.get ( "username" )
-    if username == None:
-        user = {'username': name}
-    else:
-        user = {'username': username}
-        name = username
-    return render_template ( 'index.html', title='Stream', user=user )
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'hype.mp4'))
-        return redirect(url_for('index'))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Загрузите файл с расширением mp4 </h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template ( 'index.html', title='Stream')
